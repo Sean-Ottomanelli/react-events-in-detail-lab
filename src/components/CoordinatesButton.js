aad9ee8a5d657c2314c334193acf4f20fc5b336b
github username <github email address>
2021-04-28 13:40:39 -0400

    Created button and sort of got X and Y coordinates to read

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,26 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react';
+
+
+
+class CoordinatesButton extends Component {
+    
+
+
+createXAndYArray = (evt) => {
+    let xAndYArray = []
+    xAndYArray = [evt.clientX, evt.clientY]
+    console.log(xAndYArray)
+}
+
+    render(props) {
+        return(
+            <div>
+            {this.props.onReceiveCoordinates("I'm inside the div")}
+            <button onClick = {this.createXandYArray} >Click Me!</button>
+            </div>
+        )
+    }
+}
+
+export default CoordinatesButton
